f369deca7acf333ef393e4a16726cd00d117d637
Hopetree <zlwork2014@163.com>
2019-09-08 21:25:11 +0800

    update

--- simple.groovy
+++ simple.groovy
@@ -5,6 +5,9 @@ pipeline {
     options {
         timestamps()
     }
+    environment {
+        GITHUB_USER_ID = '2b98d5a0-65f8-4961-958d-ad3620541256'
+    }
     stages {
         stage('create file') {
             steps {
@@ -12,8 +15,8 @@ pipeline {
                     def rootdir = pwd()
                     sh "cd ${rootdir} && pwd && ls -l"
                     sh "cd ${rootdir}@tmp && ls -l"
-                    git (credentialsId: "${GITHUB_USER_ID}", url: 'https://github.com/Hopetree/jenkins-demos.git', branch: "${GITHUB_BRANCH}")
-                    sh "cd "
+                    git (credentialsId: "${GITHUB_USER_ID}", url: 'https://github.com/Hopetree/jenkins-demos.git', branch: "master")
+                    sh "cd ${rootdir}/.. && ls -l"
                     sh "cd ${rootdir} && pwd && ls -l"
                     sh "cd ${rootdir}@tmp && ls -l"
                 }
