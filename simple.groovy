92b9d66d95620e67712e9380ab580e8fc1956087
Hopetree <zlwork2014@163.com>
2019-09-08 21:22:52 +0800

    update

--- simple.groovy
+++ simple.groovy
@@ -12,6 +12,10 @@ pipeline {
                     def rootdir = pwd()
                     sh "cd ${rootdir} && pwd && ls -l"
                     sh "cd ${rootdir}@tmp && ls -l"
+                    git (credentialsId: "${GITHUB_USER_ID}", url: 'https://github.com/Hopetree/jenkins-demos.git', branch: "${GITHUB_BRANCH}")
+                    sh "cd "
+                    sh "cd ${rootdir} && pwd && ls -l"
+                    sh "cd ${rootdir}@tmp && ls -l"
                 }
             }
         }
